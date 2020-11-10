ff964d2c3a3afffd780043326c930b86228843ec
Mike McCormack <nbxmike@gmail.com>
2020-11-10 10:05:53 -0500

    python scripts for serial numbers

---
+++ battery-serial-number.py
@@ -0,0 +1,51 @@
+import can
+import time
+
+def main():
+    canbus = can.interface.Bus(bustype='socketcan',
+                        channel='can0',
+                        bitrate=500000)
+    index = 0
+    time_start = time.time() 
+    prefix_found = False
+    while (prefix_found == False) and (index < 100) and ( 10 > (time.time() - time_start)) :
+        can_msg = canbus.recv(1)
+        try:
+            if (can_msg.arbitration_id >> 18) == x300:
+                prefix_found = True
+            else:
+                index = index + 1
+        except:
+            index = index & 0xffff
+    if prefix_found:
+        print("Battery serial number programmer")
+        serial_text = input("Enter serial number: ")
+        serial_number = int(serial_text)
+        serial_number = serial_number &xfffff
+        id2 = (serial_number >>16 ) & x0f
+        id1 = (serial_number >>8 ) & xff
+        id0 = serial_number & xff
+        msg = can.Message(arbitration_id=0x18000000, data=[1, id2, id1, id0, 0, 0, 0, 0], is_extended_id=True)
+        bus.send(msg)
+        
+        index = 0
+        serial_found = False
+        while (serial_found == False) and (index <100):
+            can_msg = canbus.recv(1)
+            try:
+                if (can_msg.arbitration_id >> 18) == x300:
+                    index = index + 1
+                    if (can_msg.arbitration_id & xfffff) == serial_number:
+                        prefix_found = True
+                else:
+                    index = index + 1
+            except:
+                index = index & 0xffff
+        if serial_found:
+            print("Battery serial number successfully programmed")
+    else:
+        print("No battery found")
+
+
+if __name__ == "__main__":
+    main()
